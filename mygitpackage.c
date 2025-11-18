c7177fc90e2cea12754f78619cfddc6d2874628a
Miloud Bagaa <72396190+miloudbagaa@users.noreply.github.com>
2025-11-18 16:13:33 -0500

    Update version and year in greeting message

--- mygitpackage.c
+++ mygitpackage.c
@@ -3,7 +3,7 @@
 
 int main (int argc, char *argv[]) {
 
-printf("GEI1089 - 2025 : -- My GIT package application Hello world! --\n");
+printf("GEI1089 : -- v2.0 My GIT package application Hello world! 2026 --\n");
 printf("Hi I am in GEI1089 \n");
 return 0;
 
